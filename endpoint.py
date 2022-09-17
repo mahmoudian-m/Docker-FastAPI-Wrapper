061b055d345d663acbacab934a524b06eafeead0
mostafa_mahmoudian <mahmoudian@bitbaan.com>
2022-09-17 14:26:59 +0430

    Add docker_wrapper router

--- endpoint.py
+++ endpoint.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+from api.docker_api.router import docker_wrapper
 
 app = FastAPI(title='DockerWrapper',
               description='API Wrapper for docker',
               version='0.0.1', )
+
+app.include_router(docker_wrapper)
 
 
 @app.get('/')
